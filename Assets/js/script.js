50c2f5c0cd00768a4f57107ab206c970e2d03e94
Ryan Owens <rtowens11@gmail.com>
2021-03-06 21:11:51 -0600

    initial commit

---
+++ Assets/js/script.js
@@ -0,0 +1,32 @@
+// The startGame function is called when the start button is clicked
+function startGame() {
+    isWin = false;
+    timerCount = 10;
+    // Prevents start button from being clicked when round is in progress
+    startButton.disabled = true;
+    renderBlanks()
+    startTimer()
+  }
+
+  // The setTimer function starts and stops the timer and triggers winGame() and loseGame()
+function startTimer() {
+    // Sets timer
+    timer = setInterval(function() {
+      timerCount--;
+      timerElement.textContent = timerCount;
+      if (timerCount >= 0) {
+        // Tests if win condition is met
+        if (isWin && timerCount > 0) {
+          // Clears interval and stops timer
+          clearInterval(timer);
+          winGame();
+        }
+      }
+      // Tests if time has run out
+      if (timerCount === 0) {
+        // Clears interval
+        clearInterval(timer);
+        loseGame();
+      }
+    }, 10000);
+  }
